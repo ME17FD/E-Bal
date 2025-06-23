7c923bf29014af782df44e00442a7532ce4d15ca
mehdi <faddak.mehdi@gmail.com>
2025-06-23 15:47:42 +0100

    warning fix

--- e-bal-chat-service/src/main/java/com/e_bal/e_bal_chat_service/config/WebSocketConfig.java
+++ e-bal-chat-service/src/main/java/com/e_bal/e_bal_chat_service/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.e_bal.e_bal_chat_service.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -10,15 +11,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic", "/queue", "/user");
         config.setApplicationDestinationPrefixes("/app");
         config.setUserDestinationPrefix("/user");
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat").setAllowedOriginPatterns("*");
         registry.addEndpoint("/ws-chat").setAllowedOriginPatterns("*").withSockJS();
     }
-} 
+}
